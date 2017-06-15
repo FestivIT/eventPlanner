19e4bfe42c2ffdaa88b37d6fc8104d74a9272e88
SmEiTo <gael.griffon@gmail.com>
2017-06-15 16:27:36 +0200

    Add Debounce

--- desktop/js/commun.js
+++ desktop/js/commun.js
@@ -263,3 +263,36 @@ function nl2br (str, is_xhtml) {
     var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
     return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
 }
+
+/**
+ * Retourne une fonction qui, tant qu'elle continue à être invoquée,
+ * ne sera pas exécutée. La fonction ne sera exécutée que lorsque
+ * l'on cessera de l'appeler pendant plus de N millisecondes.
+ * Si le paramètre `immediate` vaut vrai, alors la fonction 
+ * sera exécutée au premier appel au lieu du dernier.
+ * Paramètres :
+ *  - func : la fonction à `debouncer`
+ *  - wait : le nombre de millisecondes (N) à attendre avant 
+ *           d'appeler func()
+ *  - immediate (optionnel) : Appeler func() à la première invocation
+ *                            au lieu de la dernière (Faux par défaut)
+ *  - context (optionnel) : le contexte dans lequel appeler func()
+ *                          (this par défaut)
+ */
+function debounce(func, wait, immediate, context) {
+    var result;
+    var timeout = null;
+    return function() {
+        var ctx = context || this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) result = func.apply(ctx, args);
+        };
+        var callNow = immediate && !timeout;
+        // Tant que la fonction est appelée, on reset le timeout.
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) result = func.apply(ctx, args);
+        return result;
+    };
+}
